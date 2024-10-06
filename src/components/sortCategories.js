ce5e2eb09fcdadfbc788487237176be188403986
aarondon1 <issaarony@gmail.com>
2024-10-06 01:51:32 -0400

    some changes

--- src/components/sortCategories.js
+++ src/components/sortCategories.js
@@ -6,7 +6,7 @@ import { theme } from '../theme';
 
 
 export default function SortCategories() {
-    const [activeSort, setActiveSort] = useState('Popular');
+    const [activeSort, setActiveSort] = useState('Challenges');
   return (
     <View className="flex-row justify-around items-center mx-4 bg-neutral-100 rounded-full p-2 px-4 space-x-2">
       {
