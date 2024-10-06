ff9e8b1a5357c464d2c2ef8e57f4c51e3f7385cc
aarondon1 <issaarony@gmail.com>
2024-10-06 02:45:52 -0400

    latest

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,21 +1,25 @@
-import { View, Text, SafeAreaView, ScrollView, TouchableOpacity, Image, Platform, TextInput } from 'react-native'
-import React from 'react'
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
-import { MagnifyingGlassIcon } from 'react-native-heroicons/outline'
+import { View, Text, SafeAreaView, ScrollView, TouchableOpacity, Image, Platform, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { MagnifyingGlassIcon } from 'react-native-heroicons/outline';
 import Categories from '../components/categories';
 import SortCategories from '../components/sortCategories';
 import Destinations from '../components/destinations';
-const ios = Platform.OS=='ios';
-const topMargin = ios? 'mt-3': 'mt-10';
+
+const ios = Platform.OS == 'ios';
+const topMargin = ios ? 'mt-3' : 'mt-10';
+
 export default function HomeScreen() {
+  const [menuVisible, setMenuVisible] = useState(false);
+
   return (
     <SafeAreaView className="flex-1 bg-white">
-      <ScrollView showsVerticalScrollIndicator={false} className={"space-y-6 "+topMargin}>
+      <ScrollView showsVerticalScrollIndicator={false} className={"space-y-6 " + topMargin}>
         {/* avatar */}
         <View className="mx-5 flex-row justify-between items-center mb-10">
-          <Text style={{fontSize: wp(7)}} className="font-bold text-neutral-700">Let's Discover</Text>
-          <TouchableOpacity>
-            <Image source={require('../../assets/images/avatar.png')} style={{height: wp(12), width: wp(12)}} />
+          <Text style={{ fontSize: wp(7) }} className="font-bold text-neutral-700">Let's Discover</Text>
+          <TouchableOpacity onPress={() => setMenuVisible(true)}>
+            <Image source={require('../../assets/images/avatar.png')} style={{ height: wp(12), width: wp(12) }} />
           </TouchableOpacity>
         </View>
 
@@ -46,6 +50,8 @@ export default function HomeScreen() {
           <Destinations />
         </View>
       </ScrollView>
+
+      {/* Hamburger Menu */}
     </SafeAreaView>
-  )
+  );
 }
